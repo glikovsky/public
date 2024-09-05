49e531da9e0de91cd5701488c4b3c631719f17a0
glikovsky <glikovsky@gmail.com>
2024-09-05 11:19:14 +0900

    Update files

--- js/js/index.js
+++ js/js/index.js
@@ -1,36 +1,48 @@
-'use strict';
-
 $(function() {
-	const title_const = $("title").html();
+	
+	'use strict';
+	
+	let defer = $.Deferred();
 
-	let c = $(location).attr("search").replace("?c=", "");
-	if (c.length === 0) {
-		c = "home";
-	}
-	load_contents($("#" + c));
+	let conf = {}
+	$.getJSON("./conf.json", (data) => {
+		conf = data;
+		defer.resolve();
+	})
 
+	defer.promise().then(function() {
+		// URLからコンテンツを表示
+		let c = $(location).attr("search").replace("?c=", "");
+		if (c.length === 0) {
+			c = "home";
+		}
+		load_contents($("#" + c));
+	});
+
+	/* コンテンツを表示する関数
+	item: index.htmlのbody>header>nav>ulタグの中にあるli要素
+	*/
 	function load_contents(item) {
+		const title_const = conf.title;
 		let id = $(item).attr("id");
 		let title = title_const;
 		if (id !== "") {
 			title = $(item).html() + " | " + title_const;
 		}
-		$("title").html(title);
 
 		let file = "./contents/home.html";
 		if (id !== "") {
 			file = "./contents/" + id + ".html";
 		}
-		$("#main").load(file);
 
-		let url = $(location).attr("pathname");
-		if (id !== "" && id !== "home") {
-			url = "?c=" + id;
-		}
-		history.replaceState(file, title, url);
+		$("title").html(title);
+
+		$("main").load(file);
 	}
 
-	$("nav .nav_item").click(function(event) {
-		load_contents($(event.target));
-	});
+	// モバイルデバイスの場合に表示するバーガーメニューの動作
+	$(".nav_toggle").click(function() {
+		$("nav").toggleClass("nav_active");
+		$(".nav_toggle").children().toggleClass("active");
+	})
 });
